98243750829d5f0fff3d6c5f752f2e477744aa49
Steve Cervera <cervera1@mac.com>
2017-01-11 21:20:03 -0500

    updated events.rb model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
 	default_scope { order("priority ASC") }
 
 	scope :ongoing, -> { where(repeatable: true) }
-	scope :onetime, -> { where(repeatable: false) }
+	scope :onetime, -> { where(repeatable: false || nil) }
 
 end
